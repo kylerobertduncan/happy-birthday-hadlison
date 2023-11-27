7a6fd5b92186609f7756a0b46d9761314bc70a9a
Kyle <kylerobertduncan@gmail.com>
2023-11-27 16:26:45 -0500

    Adjusts opacity, improves cursor logic

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ const wrapping = document.querySelector(".wrapping");
 
 bow.addEventListener("click", () => {
   bow.classList.add("animate__rotateOutUpLeft")
+  bow.classList.remove("cursorPointer");
+  topRibbon.classList.add("cursorPointer");
+  
   topRibbon.addEventListener("click", () => {
     topRibbon.classList.add("animate__backOutLeft");
+    topRibbon.classList.remove("cursorPointer");
+    bottomRibbon.classList.add("cursorPointer");
+  
     bottomRibbon.addEventListener("click", () => {
       bottomRibbon.classList.add("animate__backOutUp");
+      bottomRibbon.classList.remove("cursorPointer");
+      wrapping.classList.add("cursorPointer");
+  
       wrapping.addEventListener("click", () => {
         wrapping.classList.add("animate__fadeOutDown");
+        wrapping.classList.remove("cursorPointer");
       });
     });
   });
